ade9d02365985a5a01402654528d807c4659a196
Selina Musuta <selina@codeforprogress.org>
2016-09-16 17:08:44 -0400

    learning formatters

---
+++ LearnPythonTheHardWay/exe4.py
@@ -0,0 +1,31 @@
+# There are 100 cars
+cars = 100
+
+# Each car has 4 spaces
+space_in_a_car = 4.0
+
+# the # of drivers
+drivers = 30
+
+# the # of passengers
+passengers = 90
+
+# There are 30 drivers
+cars_driven = drivers
+
+# How many cars were not driven
+cars_not_driven = cars - drivers
+
+# Capacity of the cars
+carpool_capacity = cars_driven * space_in_a_car
+
+# Average of passengers per car
+average_passengers_per_car = passengers / cars_driven
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
+print "i am alive"
