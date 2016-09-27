40309c940af690ed37441fa959959ee6c2718521
Selina Musuta <selina@codeforprogress.org>
2016-09-27 17:52:07 -0400

    exercise 13 added about passing arguments using argv

---
+++ exe13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, stuff, things, that = argv
+
+print "The script is called:", script
+print "Your first variable is:", stuff
+print "Your second variable is:", things
+print "Your third variable is:", that
+
+
