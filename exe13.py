9986f4fb0211009113290679aedafc6437fbe72b
Selina Musuta <selina@codeforprogress.org>
2016-09-27 18:09:05 -0400

    exercise 13 modified with raw_input
    exit
    q

--- exe13.py
+++ exe13.py
@@ -2,6 +2,10 @@ from sys import argv
 
 script, stuff, things, that = argv
 
+name = raw_input("What is your name?")
+
+print "So your name is %r." % name
+
 print "The script is called:", script
 print "Your first variable is:", stuff
 print "Your second variable is:", things
