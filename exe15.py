96b4e2b52483de31c8ed0d5406777d732df77462
Selina Musuta <selina@codeforprogress.org>
2016-09-28 10:29:25 -0400

    Add read/write exercises

---
+++ exe15.py
@@ -0,0 +1,22 @@
+# Get the systems package from the arguments variable module
+from sys import argv
+
+# Create two argument variables
+script, filename = argv
+
+# Create a txt variable to display text 
+txt = open(filename)
+
+# Print string to display file text
+print "Here's your file %r:" % filename
+
+# Read text
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+# Remember to call the object ()...without it I'm only calling the object reference
+print txt_again.read()
