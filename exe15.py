1331361d049525a1d2f22750a000f0e9ee92e8cb
Selina Musuta <selina@codeforprogress.org>
2016-09-29 16:58:05 -0400

    add exercises 15 and 16...read/write

--- exe15.py
+++ exe15.py
@@ -5,7 +5,7 @@ from sys import argv
 script, filename = argv
 
 # Create a txt variable to display text 
-txt = open(filename)
+txt = close(filename)
 
 # Print string to display file text
 print "Here's your file %r:" % filename
@@ -16,7 +16,7 @@ print txt.read()
 print "Type the filename again:"
 file_again = raw_input("> ")
 
-txt_again = open(file_again)
+txt_again = close(file_again)
 
 # Remember to call the object ()...without it I'm only calling the object reference
 print txt_again.read()
