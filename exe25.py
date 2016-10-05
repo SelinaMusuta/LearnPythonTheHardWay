7aa854cad50e391e64bb4c59429d550423a0daaf
Selina Musuta <selina@codeforprogress.org>
2016-10-05 15:46:34 -0400

    correct exercise 26...mostly syntax errors

--- exe25.py
+++ exe25.py
@@ -25,9 +25,12 @@ def sort_sentence(sentence):
 def print_first_and_last(sentence):
 	"""Prints the first and last words of the sentence."""
 	words = break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
 
 def print_first_and_last_sorted(sentence):
 	"""Sorts the words then prints the first and last one"""
 	words = sort_sentence(sentence)
 	print_first_word(words)
 	print_last_word(words)
+	
